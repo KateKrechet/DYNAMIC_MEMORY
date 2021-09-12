ab132471b380f49626ec30b7bd03048beb5fd35c
KateKrechet <k.krechet@yandex.ru>
2021-09-12 19:25:47 +0300

    Add project files.

---
+++ Dynamic_Memory/Source.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void FillRand(int arr[], const unsigned int n);
+void Print(int arr[], const unsigned int n);
+int* push_back(int arr[], int& n, int value);
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	int value;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr=push_back(arr, n, value);
+	//n++;
+	//Print(arr, n);
+	//buffer[2] = 1024;
+	Print(arr, n);
+	delete[]arr;
+}
+void FillRand(int arr[], const unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void Print(int arr[], const unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+
+	}
+	cout << endl;
+}
+int* push_back(int arr[], int& n, int value)
+{
+	//добавление элемента в массив
+	//1)создаем буферный массив нужного размера
+	int* buffer = new int[n + 1];
+	//копируем исходный массив в буферный
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3)удалить исходный массив
+	delete[] arr;
+	//4)подменем исходный массив новым (буферным) массивом, за счет подмены адреса
+	arr = buffer;
+	//5)Только после всех этих действий можно добавить значние в конец массива
+	arr[n] = value;
+	//6)После добавления элемента в массив количество его элементов увеличивается на 1
+	n++;
+	return arr;
+}
